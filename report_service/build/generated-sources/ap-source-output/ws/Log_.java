a6a0efc84a47a77353633b78f3fbca37811760d1
jonattanandrade <jonattan.andrade@javeriana.edu.co>
2017-09-28 13:21:53 -0500

    inicial

---
+++ report_service/build/generated-sources/ap-source-output/ws/Log_.java
@@ -0,0 +1,14 @@
+package ws;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.1.v20130918-rNA", date="2017-09-28T12:38:27")
+@StaticMetamodel(Log.class)
+public class Log_ { 
+
+    public static volatile SingularAttribute<Log, String> log;
+    public static volatile SingularAttribute<Log, Integer> id;
+
+}
---
+++ report_service/src/java/ws/Log.java
@@ -0,0 +1,92 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ws;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author Cata
+ */
+@Entity
+@Table(name = "log")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Log.findAll", query = "SELECT l FROM Log l"),
+    @NamedQuery(name = "Log.findById", query = "SELECT l FROM Log l WHERE l.id = :id"),
+    @NamedQuery(name = "Log.findByLog", query = "SELECT l FROM Log l WHERE l.log = :log")})
+public class Log implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Integer id;
+    @Size(max = 2147483647)
+    @Column(name = "log")
+    private String log;
+
+    public Log() {
+    }
+
+    public Log(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getLog() {
+        return log;
+    }
+
+    public void setLog(String log) {
+        this.log = log;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Log)) {
+            return false;
+        }
+        Log other = (Log) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "ws.Log[ id=" + id + " ]";
+    }
+    
+}
